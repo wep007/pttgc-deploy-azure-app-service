a595142fef804af98ddf8310c0a51fd9d20edcff
Prawit Chaivong <pchaivong@gmail.com>
2022-09-20 07:56:20 +0700

    Add AppInfo API

---
+++ PTTGCSampleApp/Controllers/InfoController.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using PTTGCSampleApp.Models;
+
+namespace PTTGCSampleApp.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class InfoController : ControllerBase
+    {
+
+        [HttpGet()]
+        public IActionResult Info()
+        {
+            AppInfo i = new AppInfo();
+            i.AppName = "simple-service";
+            i.Status = "OK";
+
+            return new OkObjectResult(i);
+        }
+    }
+}
---
+++ PTTGCSampleApp/Models/AppInfo.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace PTTGCSampleApp.Models
+{
+    public class AppInfo
+    {
+        public String AppName { get; set; }
+        public String Status { get; set; }
+    }
+}
